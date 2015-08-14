bd14be4457a354dc87d0098a72bcee384ca5e030
Zhibo Chen <a1s1d1f1j1k1l1@126.com>
2015-08-14 14:26:14 +0800

    Initial Commit

---
+++ Chinese Traditional Gaming/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chinese Traditional Gaming
+//
+//  Created by Chen Zhibo on 8/14/15.
+//  Copyright © 2015 Chen Zhibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
